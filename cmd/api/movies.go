4c028cd554d4966eff5d84ecd8f945daf8204aa0
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2024-05-14 14:30:04 -0500

    helpers readIDParam added

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint
@@ -15,18 +12,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// When httprouter is parsing a request, any interpolated URL parameters will be
-	// stored in the request context. We can use the ParamsFromContext() function to
-	// retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// We can use the ByName() method to get the value of the "id" parameter from
-	// the slice. In our project all movies will have a unique positive integer ID,
-	// but the value returned by ByName() is always a string. So we try to convert it to a
-	// base 10 integer. If the parameter couldn't be converted, or is less than 1, we know
-	// the ID is invalid so we use the http.NotFound() fucntion to return a 404 Not Found.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
